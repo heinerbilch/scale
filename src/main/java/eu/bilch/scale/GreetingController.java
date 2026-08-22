9315a288f2c365fcd52b21f673a1ca7595fecf8a
Heiner Strauß <heiner_ej@yahoo.de>
2026-08-22 17:37:25 +0200

    renames com to eu

--- src/main/java/eu/bilch/scale/GreetingController.java
+++ src/main/java/eu/bilch/scale/GreetingController.java
@@ -1,6 +1,8 @@
-package com.bilch.scale;
+package eu.bilch.scale;
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +12,13 @@ public class GreetingController {
 
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
+  private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
   @GetMapping("/api/greeting")
   public String greeting(@RequestParam(defaultValue = "World") String name) {
-    return template.formatted(name);
+    counter.incrementAndGet();
+    String response = template.formatted(name);
+    logger.debug("Returning greeting #{}: {}", counter, response);
+    return response;
   }
 }
