ff57feef0176678100bce8ec212ee19c0d3e36be
Heiner Strauß <heiner_ej@yahoo.de>
2026-08-28 17:09:33 +0200

    cleans up

--- src/main/java/eu/bilch/scale/controller/BlogController.java
+++ src/main/java/eu/bilch/scale/controller/BlogController.java
@@ -8,8 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import eu.bilch.scale.model.Post;
 import eu.bilch.scale.repository.PostRepository;
 
