d0908a8380717e008442520cf3a67cfc10de1c3f
Heiner Strauß <heiner_ej@yahoo.de>
2026-09-10 20:38:54 +0200

    fixes default oauth settings, adds logging when creating a new user vi oauth

--- src/main/java/eu/bilch/scale/config/CustomOAuth2UserService.java
+++ src/main/java/eu/bilch/scale/config/CustomOAuth2UserService.java
@@ -4,6 +4,8 @@ import eu.bilch.scale.model.User;
 import eu.bilch.scale.repository.UserRepository;
 import jakarta.transaction.Transactional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -20,33 +22,35 @@ import java.util.Map;
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
     private final UserRepository userRepository;
-
-    public CustomOAuth2UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
-
-    @Override
-    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-        OAuth2User oAuth2User = super.loadUser(userRequest);
-        
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-        String email = (String) attributes.get("email");
-        String googleId = (String) attributes.get("sub");
-        String firstName = (String) attributes.get("given_name");
-        String lastName = (String) attributes.get("family_name");
-        String picture = (String) attributes.get("picture");
-        
-        // User in Datenbank speichern oder aktualisieren
-        User user = userRepository.findByGoogleId(googleId)
-                .orElseGet(() -> {
-                    User newUser = new User();
-                    newUser.setEmail(email);
-                    newUser.setGoogleId(googleId);
-                    newUser.setFirstName(firstName);
-                    newUser.setLastName(lastName);
-                    newUser.setProfilePicture(picture);
-                    newUser.setUsername(email.split("@")[0]); // Benutzername aus Email
-                    newUser.setCreatedAt(LocalDateTime.now());
+        private Logger log = LoggerFactory.getLogger(CustomOAuth2UserService.class);
+    
+        public CustomOAuth2UserService(UserRepository userRepository) {
+            this.userRepository = userRepository;
+        }
+    
+        @Override
+        public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+            OAuth2User oAuth2User = super.loadUser(userRequest);
+            
+            Map<String, Object> attributes = oAuth2User.getAttributes();
+            String email = (String) attributes.get("email");
+            String googleId = (String) attributes.get("sub");
+            String firstName = (String) attributes.get("given_name");
+            String lastName = (String) attributes.get("family_name");
+            String picture = (String) attributes.get("picture");
+            
+            // User in Datenbank speichern oder aktualisieren
+            User user = userRepository.findByGoogleId(googleId)
+                    .orElseGet(() -> {
+                        User newUser = new User();
+                        newUser.setEmail(email);
+                        newUser.setGoogleId(googleId);
+                        newUser.setFirstName(firstName);
+                        newUser.setLastName(lastName);
+                        newUser.setProfilePicture(picture);
+                        newUser.setUsername(email.split("@")[0]); // Benutzername aus Email
+                        newUser.setCreatedAt(LocalDateTime.now());
+                        log.info("New user {}", newUser);
                     return userRepository.save(newUser);
                 });
 
